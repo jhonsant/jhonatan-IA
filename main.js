1dcf28d7d67d227464432442b1d8a8c6a1681dee
jhonsant <141195205+jhonsant@users.noreply.github.com>
2024-05-24 12:19:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 const caixaPrincipal =document.querySelector('caixa-principal');
+const caixaPerguntas =document.querySelector('caixa-perguntas');
+const caixaAlternativa =document.querySelector('caixa-alternativas');
+const caixaResultado =document.querySelector('caixa-resultado');
+const textoResultado =document.querySelector('texto-resultado');
+
+const perguntas = [
+{
+    enunciado: "Insira o enunciado da Pergunta 1",
+    alternativas: ["Alternativas 1","Alternativas 2"]
+},  
+{
+    enunciado: "Insira o enunciado da Pergunta 2",
+    alternativas: ["Alternativa 1","Alternativa 2"]
+}  
+]
